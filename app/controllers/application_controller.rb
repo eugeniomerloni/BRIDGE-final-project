2d652743063bed341043768b573497e8e613b128
eugeniomerloni <164801392+eugeniomerloni@users.noreply.github.com>
2024-05-24 22:48:20 +0000

    completed sign-up process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:hometown])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:hometown])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :dob_day, :dob_month, :dob_year, :hometown])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :dob_day, :dob_month, :dob_year, :hometown])
   end
 end
